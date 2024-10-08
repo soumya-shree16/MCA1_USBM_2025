fa54fa73f8a3520540f213464cc7d8b61b6b296e
soumyashreebehera <soumyashree161@gmail.com>
2024-10-08 11:47:50 +0530

    task added

---
+++ frontened/Javascript/class3/live/math.js
@@ -0,0 +1,32 @@
+//mathematically operator
+// let n1=56;
+// let n2=8;
+// let res=n1+n2;
+// let sub=n1-n2;
+// let mul=n1*n2;
+// let div=n1/n2;
+// console.log(res,sub,mul,div)
+
+// //to check even or odd 
+// let num=45
+// let even=num%2
+// console.log(even)
+
+// //exponential operator
+// let sq=2**2
+// let cu=2**3
+// console.log(sq,cu)
+
+//comparision
+//console.log(10>!5)
+
+
+
+let totalBill=850
+discount_start=1000
+if(totalBill>discount_start){
+    console.log("discount");
+}
+else{
+    console.log("not discount");
+}
